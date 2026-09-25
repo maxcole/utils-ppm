2287e3c6412bf0cdf9042f42c638a3bd6defb8f8
Robert Roach <rjayroach@gmail.com>
2026-09-25 02:43:01 -0400

    mosh update

---
+++ packages/mosh/home/.config/zsh/mosh.zsh
@@ -0,0 +1,6 @@
+# mosh.zsh
+
+mosh() {
+  # command mosh --server=/opt/homebrew/bin/mosh-server $1
+  command mosh --server=/home/linuxbrew/.linuxbrew/bin/mosh-server $1
+}
--- packages/mosh/home/.config/zsh/tailscale.zsh
+++
@@ -1,3 +0,0 @@
-# tailscale.zsh
-
-zcomp tailscale
